4ba60eb7b2f4df8f335c9bbf7c808dbc68993a7d
Notmemayeb <notmemaybeatwork@gmail.com>
2022-11-22 18:57:45 +0100

    Aeropuerto class fixed

--- src/Aeropuerto.java
+++ src/Aeropuerto.java
@@ -1,13 +1,11 @@
 /**
  * Description of the class
  *
- * @author
+ * @author  Fedor Kunin
  * @author
  * @version     1.0
  */
 public class Aeropuerto {
-
-
 
     /**
      * Constructor of the class
@@ -19,22 +17,39 @@ public class Aeropuerto {
      * @param terminales
      */
 
-    private String nombre;
+    private static String nombre;
     private String codigo;
-    double latitud, longitud;
+    private double latitud, longitud;
+    private int terminales;
 
-    public Aeropuerto(String nombre, String codigo, double latitud, double longitud, int terminales);
-    public String getNombre();
-    public String getCodigo();
-    public double getLatitud();
-    public double getLongitud();
-    public int getTerminales();
+
+    public Aeropuerto(String nombre, String codigo, double latitud, double longitud, int terminales){
+
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.terminales = terminales;
+
+    };
+    public String getNombre(){ return nombre; };
+    public String getCodigo(){ return codigo; };
+    public double getLatitud(){ return latitud; };
+    public double getLongitud(){ return longitud; };
+    public int getTerminales(){ return terminales; };
+
     // Calcula la distancia entre el aeropuerto que recibe el mensaje y el aeropuerto "destino" siguiendo la fórmula del enunciado
-    public double distancia(Aeropuerto destino);
+    public double distancia(Aeropuerto destino){
+        return 1;
+    };
     // Crea un String con los datos de un aeropuerto con el siguiente formato:
     // Adolfo Suarez Madrid - Barajas(MAD), en (40.4927751 -3.5778), con 4 terminales
-    public String toString();
+    public String toString(){
+        return "";
+    };
     // Crea un String con los datos de un aeropuerto con el siguiente formato:
     // Adolfo Suarez Madrid - Barajas(MAD)
-    public String toStringSimple();
+    public String toStringSimple(){
+        return "";
+    };
 }
