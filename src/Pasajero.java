3b6fd6424e12168ede4d90c4cd0594f694757164
notmemayeb <notmemaybeatwork@gmail.com>
2022-11-23 16:03:01 +0100

    maxBilletes fix

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -50,10 +50,6 @@ public class Pasajero {
         return 1;
     };
     public boolean maxBilletesAlcanzado(){
-        boolean aux;
-        if () {
-            aux = true;
-        }
         return false;
     };
     public Billete getBillete(int i){
