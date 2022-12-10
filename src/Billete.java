6ffd0db5c48dec71ab7810dcc706997248f5403d
isaac <isaaclopezvalverde@gmail.com>
2022-12-10 12:42:10 +0100

    Billete.generarFactura added

--- src/Billete.java
+++ src/Billete.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -81,6 +83,51 @@ Billete {
     };
     // Imprime la informacion de este billete en un fichero siguiendo el formato de los ejemplos de ejecución del enunciado
     public boolean generarFactura(String fichero){
+        PrintWriter salida = null;
+        try {
+            salida = new PrintWriter(fichero);
+            salida.printf(
+                            "--------------------------------------------------\n" +
+                            "--------- Factura del billete %s ---------\n" +
+                            "--------------------------------------------------\n" +
+                            "Vuelo: %s\n" +
+                            "Origen: %s (%s) T%d\n" +
+                            "Destino: %s (%s) T%d\n" +
+                            "Salida: %s\n" +
+                            "Llegada: %s\n" +
+                            "Pasajero: %s %s, %s, %s\n" +
+                            "Tipo de billete: %s\n" +
+                            "Asiento: %s\n" +
+                            "Precio: %f€",
+                    vuelo.getID(),
+                    vuelo.getOrigen().getCodigo(),
+                    vuelo.getOrigen().getNombre(),
+                    vuelo.getOrigen().getCodigo(),
+                    vuelo.getOrigen().getTerminales(),
+                    vuelo.getDestino().getNombre(),
+                    vuelo.getDestino().getCodigo(),
+                    vuelo.getDestino().getTerminales(),
+                    vuelo.getSalida(),
+                    vuelo.getLlegada(),
+                    pasajero.getNombre(),
+                    pasajero.getApellidos(),
+                    pasajero.getDNI(),
+                    pasajero.getEmail(),
+                    getTipo().toString(),
+                    getAsiento(),
+                    getPrecio());
+        } catch (Exception _exc){
+            System.out.printf("Error de escritura de fichero %s.",fichero);
+        } finally {
+            if (salida != null){
+                try {
+                    salida.close();
+                    System.out.printf("Facturada de Billete %s generada en %s", vuelo.getID(), fichero);
+                } catch (Exception _exc){
+                    System.out.printf("Error de cierre de fichero %s.",fichero);
+                }
+            }
+        }
         return true;
     };
 
